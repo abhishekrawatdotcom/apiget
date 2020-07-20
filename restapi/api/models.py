0abad0fdc980d578cfd0c61d8eae8d111337f8cd
abhishekrawatdotcom <64519433+abhishekrawatdotcom@users.noreply.github.com>
2020-07-20 10:38:06 +0530

    Update models.py
    changes code

--- restapi/api/models.py
+++ restapi/api/models.py
@@ -2,4 +2,7 @@ from django.db import models
 class apimodel(models.Model):
     name=models.CharField(max_length=50)
     address=models.CharField(max_length=50)
+    
+    add=models.CharField(max_length=50)
+    
 
